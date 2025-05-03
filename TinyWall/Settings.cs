09b9d4f7dd24c281683ceb8127a97b9203323d45
DaPa <at5dapa1@gmail.com>
2025-05-03 20:45:27 +0300

    Change some defaults

--- TinyWall/Settings.cs
+++ TinyWall/Settings.cs
@@ -60,7 +60,7 @@ namespace pylorak.TinyWall
 
         // Manage window
         [DataMember(EmitDefaultValue = false)]
-        public bool AskForExceptionDetails = false;
+        public bool AskForExceptionDetails = true;
         [DataMember(EmitDefaultValue = false)]
         public int SettingsTabIndex;
         [DataMember(EmitDefaultValue = false)]
@@ -72,7 +72,7 @@ namespace pylorak.TinyWall
 
         // Hotkeys
         [DataMember(EmitDefaultValue = false)]
-        public bool EnableGlobalHotkeys = true;
+        public bool EnableGlobalHotkeys = false;
 
         [OnDeserialized]
         private void OnDeserialized(StreamingContext sc)
